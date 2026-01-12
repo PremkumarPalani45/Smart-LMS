dc08c1a6368191029bb1e12df63d87f4f78940ee
Prem Kumar <premrohit45333@gmail.com>
2026-01-13 00:18:21 +0530

    another bug fix

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,20 @@ const app = express();
 const port = process.env.PORT || 3003;
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://thriving-truffle-c6c269.netlify.app"
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
